2fe460adfb07f84c785b9b2c1d099e51fab2dbf0
SifatSararChistee <sifatsararchistee@gamil.com>
2024-10-06 10:48:27 +0600

    added categories

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
-console.log("connected");
+const loadCatagories = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/peddy/categories"
+  );
+  const data = await response.json();
+  displayCatagories(data.categories);
+};
 
-function scrollToSection(sectionId) {
-  document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
-}
+const displayCatagories = (categories) => {
+  categories.forEach((categoryItem) => {
+    const { category, category_icon } = categoryItem;
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <button class="btn w-[200px] h-[60px] p-6"><img class="w-8" src="${category_icon}" alt="">${category}</button>
+    `;
+    document.getElementById("catagories").appendChild(div);
+  });
+};
+
+loadCatagories();
