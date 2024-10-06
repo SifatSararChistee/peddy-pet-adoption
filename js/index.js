334eae6a7e1b1ea1ecf10489220d2d0625bf1c5f
SifatSararChistee <sifatsararchistee@gamil.com>
2024-10-06 15:51:58 +0600

    added no info

--- js/index.js
+++ js/index.js
@@ -40,46 +40,56 @@ const loadAllCards = async () => {
 
 //Display all cards
 const displayCards = (card) => {
-  document.getElementById("cards-container").innerHTML = "";
-
-  card.forEach((item) => {
-    const { pet_name, image, gender, date_of_birth, breed, price, category } =
-      item;
+  if (card.length === 0) {
+    document.getElementById("cards-container").innerHTML = "";
     const div = document.createElement("div");
+    div.classList.add("flex", "justify-center", "items-center", "flex-col");
     div.innerHTML = `
-                  <div class="card bg-base-100 w-[300px] shadow-xl">
-                <figure class="px-6 pt-10">
-                  <img
-                    src=${image}
-                    alt="Shoes"
-                    class="rounded-xl" />
-                </figure>
-                <div class="card-body items-start">
-                  <h2 class="card-title">${
-                    pet_name == null ? "Not Mentioned" : pet_name
-                  }</h2>
-                  <p><i class="fa-solid fa-list mr-2"></i>Breed: ${
-                    breed == null ? "Not Mentioned" : breed
-                  }</p>
-                  <p><i class="fa-regular fa-calendar mr-2"></i>Birth: ${
-                    date_of_birth == null ? "Not Mentioned" : date_of_birth
-                  }</p>
-                  <p><i class="fa-solid fa-mercury mr-2"></i>Gender: ${
-                    gender == null ? "Not Mentioned" : gender
-                  }</p>
-                  <p><i class="fa-solid fa-dollar-sign mr-2"></i>Price: ${
-                    price == null ? "Not Mentioned" : price
-                  }</p>
-                  <div class="flex justify-between w-full">
-                    <button class="btn"><i class="fa-regular fa-thumbs-up"></i></button>
-                    <button class="btn text-[#0E7A81]">Adopt</button>
-                    <button class="btn text-[#0E7A81]">Details</button>
-                  </div>
-                </div>
-              </div>
+    <img src="assets/error.webp" alt="">
+    <h1 class="font-bold text-3xl">No Information Available</h1>
+    <p>We’re sorry, but the information you’re looking for is currently unavailable. Please check back later or explore other sections of our website. If you need immediate assistance, feel free to contact our support team.</p>
     `;
     document.getElementById("cards-container").append(div);
-  });
+  } else {
+    document.getElementById("cards-container").innerHTML = "";
+    card.forEach((item) => {
+      const { pet_name, image, gender, date_of_birth, breed, price } = item;
+      const div = document.createElement("div");
+      div.innerHTML = `
+                    <div class="card bg-base-100 w-[300px] shadow-xl">
+                  <figure class="px-6 pt-10">
+                    <img
+                      src=${image}
+                      alt="Shoes"
+                      class="rounded-xl" />
+                  </figure>
+                  <div class="card-body items-start">
+                    <h2 class="card-title">${
+                      pet_name == null ? "Not Mentioned" : pet_name
+                    }</h2>
+                    <p><i class="fa-solid fa-list mr-2"></i>Breed: ${
+                      breed == null ? "Not Mentioned" : breed
+                    }</p>
+                    <p><i class="fa-regular fa-calendar mr-2"></i>Birth: ${
+                      date_of_birth == null ? "Not Mentioned" : date_of_birth
+                    }</p>
+                    <p><i class="fa-solid fa-mercury mr-2"></i>Gender: ${
+                      gender == null ? "Not Mentioned" : gender
+                    }</p>
+                    <p><i class="fa-solid fa-dollar-sign mr-2"></i>Price: ${
+                      price == null ? "Not Mentioned" : price
+                    }</p>
+                    <div class="flex justify-between w-full">
+                      <button onclick="likeBtn()" class="btn"><i class="fa-regular fa-thumbs-up"></i></button>
+                      <button class="btn text-[#0E7A81]">Adopt</button>
+                      <button class="btn text-[#0E7A81]">Details</button>
+                    </div>
+                  </div>
+                </div>
+      `;
+      document.getElementById("cards-container").append(div);
+    });
+  }
 };
 
 loadCatagories();
