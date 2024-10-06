df0c98759c757bb7f2cc1d763fe17e511621a2a4
SifatSararChistee <sifatsararchistee@gamil.com>
2024-10-06 11:08:43 +0600

    added cards section

--- js/index.js
+++ js/index.js
@@ -10,10 +10,11 @@ const displayCatagories = (categories) => {
   categories.forEach((categoryItem) => {
     const { category, category_icon } = categoryItem;
     const div = document.createElement("div");
+    div.classList.add("flex", "justify-center", "items-center", "p-6");
     div.innerHTML = `
-        <button class="btn w-[200px] h-[60px] p-6"><img class="w-8" src="${category_icon}" alt="">${category}</button>
+        <button class="btn w-[150px]"><img class="w-8" src="${category_icon}" alt="">${category}</button>
     `;
-    document.getElementById("catagories").appendChild(div);
+    document.getElementById("catagories").append(div);
   });
 };
 
